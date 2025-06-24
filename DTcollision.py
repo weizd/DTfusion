9794dc5f3f93962e22629c02d2d809d99b5468c7
Wangxy123456 <2993660940@qq.com>
2025-06-24 14:37:23 +0800

    修改代码错误，正确保存模型

--- DTcollision.py
+++ DTcollision.py
@@ -526,22 +526,21 @@ if __name__ == '__main__':
     model = PINN3DSpherical(layers)
     solver = Solver3DSpherical(model, X0, U0, V0, X_f, arrays, mean_density, X_f, U2, V2)
 
-    # 训练、验证网络
-    folder_path = os.getcwd()   # 获取当前程序路径
-    file_name = 'save_model.pkl'    # 保存的训练模型名称
-    file_path = folder_path + '/save_model/' + file_name
+    # 未训练模型 进行模型训练
+    history = solver.train(epochs=2, lr=1e-3, initial_batch_size=3000)
+    # 可视化损失
+    plot_history(history)
 
-    if os.path.exists(file_path) and os.path.isfile(file_path):
-        # 已经训练完成 直接加载模型
-        # model.load_state_dict(torch.load(file_path))
-    # else:
-        # 未训练模型 进行模型训练
-        history = solver.train(epochs=2, lr=1e-3, initial_batch_size=3000)
-        # 可视化损失
-        plot_history(history)
-        # 保存模型
-        torch.save(model.state_dict(), file_path)
-
+    # 确保目录存在
+    file_path = os.path.join("E:/weizd/gitProject/DTfusion/save_model", "save_model.pkl")
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    # 保存模型
+    torch.save(model.state_dict(), file_path)
+    print(f"Save model weights to {file_path}")
+    # if os.path.exists(file_path) and os.path.isfile(file_path):
+    #     # 已经训练完成 直接加载模型
+    #     model.load_state_dict(torch.load(file_path))
+    #     print(f"Loaded model weights from {file_path}")
 
     # 测试点 只看 φ=pi/6; θ=0; t=0.5截面
     predict_and_plot(solver, R, k, R0)
